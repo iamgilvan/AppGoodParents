77faf8e2c5ccaeb41884ef9f295dccd382eab91e
Gilvan Praxedes de Almeida <g6almeida@hotmail.com>
2018-11-17 21:05:30 -0200

    Ajustes para build.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,16 @@ import { Camera } from '@ionic-native/camera'
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
-import { CadastroPage } from '../pages/cadastro/cadastro';
+import { CadastroPage } from "../pages/cadastro/cadastro";
 import { DetalhePage } from '../pages/detalhe/detalhe';
 import { MenuPage } from '../pages/menu/menu';
 import { EditarPage } from '../pages/editar/editar';
+
+import { DetalhePageModule } from '../pages/detalhe/detalhe.module';
+import { MenuPageModule } from '../pages/menu/menu.module';
+import { EditarPageModule } from '../pages/editar/editar.module';
+import { LoginPageModule} from '../pages/login/login.module';
+import { CadastroPageModule } from "../pages/cadastro/cadastro.module";
 
 import { UsersProvider } from '../providers/users/users';
 import { LoginProvider } from '../providers/login/login';
@@ -21,18 +27,18 @@ import { LoginProvider } from '../providers/login/login';
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
-    LoginPage,
-    CadastroPage,
-    DetalhePage,
-    MenuPage,
-    EditarPage
+    HomePage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
     HttpClientModule,
-    HttpModule
+    HttpModule,
+    CadastroPageModule,
+    DetalhePageModule,
+    EditarPageModule,
+    LoginPageModule,
+    MenuPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
